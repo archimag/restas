4b05a868bc902db3ed882f87e2a5d84ba11b4c26
Moskvitin Andrey <archimag@gmail.com>
2009-10-16 19:08:39 +0400

    update examples

--- example/example-1.lisp
+++ example/example-1.lisp
@@ -1,63 +1,29 @@
-;;; example.lisp
-
+(asdf:operate 'asdf:load-op :cl-who)
 (asdf:operate 'asdf:load-op :restas)
-(asdf:operate 'asdf:load-op :xfactory)
 
-(restas:define-plugin :restas.example-1
+(restas:defsite :restas.example-1
   (:use :cl))
 
 (in-package :restas.example-1)
 
-(defun in-pool (obj)
-  (gp:object-register obj *request-pool*))
-
-(defparameter *master* "<?xml version=\"1.0\" encoding=\"utf-8\"?>
-<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">
-
-<html xmlns=\"http://www.w3.org/1999/xhtml\">
-	<head >
-        <meta http-equiv=\"Content-Type\" content=\"text/html;charset=utf-8\" />
-        <title>Example 1</title>
-	</head>
-
-	<body>
-       <h1>Welcome!</h1>
-       <div id=\"content\" />
-    </body>
-</html> ")
+(restas:define-site-plugin  default (:restas.example-1))
 
 
-(define-simple-route main ("" :method :get :overlay-master *master*)
-  (in-pool
-   (xfactory:with-document-factory ((html))
-     (html "overlay"
-           (html :div
-                 (xfactory:attributes :id "content")
-                 (html :form
-                       (xfactory:attributes :method "post")
-                       (html :div "Input test message:")
-                       (html :input
-                             (xfactory:attributes :name "message"))
-                       (html :input
-                             (xfactory:attributes :value "Send"
-                                                  :type "submit"))))))))
-   
-(define-simple-route main/post ("" :method :post :overlay-master *master*)
-  (in-pool
-   (xfactory:with-document-factory ((html))
-     (html "overlay"
-           (html :div
-                 (xfactory:attributes :id "content")
-                 (html :div
-                       (html :b "test message: ")
-                       (xfactory:text (hunchentoot:post-parameter "message")))
-                 (html :a
-                       (xfactory:attributes :href "")
-                       "Try again"))))))
+(define-route main ("" :method :get)
+  (who:with-html-output-to-string (out)
+    (:html
+     (:body
+      ((:form :method :post)
+       ((:input :name "message"))
+       ((:input :type "submit" :value "Send")))))))
 
+(define-route main/post ("" :method :post)
+  (who:with-html-output-to-string (out)
+    (:html
+     (:body
+      (:div
+       (:b (who:fmt "test message: ~A"
+                    (hunchentoot:post-parameter "message"))))
+      ((:a :href (restas:genurl 'main)) "Try again")))))
 
-(restas:reconnect-all-plugins)
-(restas:start-web-server 8080)
-
-
-  
+(restas:start-site :restas.example-1 :port 8080)
--- example/example-2.lisp
+++ example/example-2.lisp
@@ -1,108 +1,52 @@
-;;; example.lisp
+;;;; example 2
 
+(asdf:operate 'asdf:load-op :cl-who)
 (asdf:operate 'asdf:load-op :restas)
-(asdf:operate 'asdf:load-op :xfactory)
 
-(restas:define-plugin :restas.example-2
-  (:use :cl))
+(restas:defsite :restas.example-2
+  (:use :cl :iter))
 
 (in-package :restas.example-2)
 
-(defparameter *frame* "<?xml version=\"1.0\" encoding=\"utf-8\"?>
-<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd\">
+(restas:define-site-plugin  default (:restas.example-2))
 
-<html xmlns=\"http://www.w3.org/1999/xhtml\">
-	<head >
-        <meta http-equiv=\"Content-Type\" content=\"text/html;charset=utf-8\" />
-	</head>
+(define-route root ("")
+  (who:with-html-output-to-string (out)
+    (:html
+     (:head
+      (:title "Example 2: Index"))
+     (:body
+      (:h1 "Index")
+      (:ul
+       (iter (for x from 1 to 10)
+             (who:htm (:li
+                       ((:a :href (genurl 'chapter-?.html :id x))
+                        (who:fmt "Chapter ~A" x))))))))))
 
-	<body>
-       <div id=\"content\" />
-    </body>
-</html> ")
+(define-route chapter-?.html ("chapter-:(id).html")
+  (who:with-html-output-to-string (out)
+    (:html
+     (:head
+      (:title (who:fmt "Example 2. Chapter ~A" id)))
+     (:body
+      (:h1 (who:fmt "Chapter ~A" id))
+      (:ul
+       (iter (for x from 1 to 10)
+             (who:htm (:li
+                       ((:a :href (genurl 'chapter-?-?.html :id1 id :id2 x))
+                        (who:fmt "Chapter ~A-~A" id x))))))
+      ((:a :href (genurl 'root))
+       "Back to Index")))))
 
-(defun in-pool (obj)
-  (gp:object-register obj *request-pool*))
-
-(defun apply-format-aux (format args)
-  (if args
-      (apply #'format nil (cons format args))
-      format))
-
-(defun eid (format &rest args)
-  "Make id attribute"
-  (xfactory:attributes :id
-                       (apply-format-aux format args)))
-
-(defun ehref (format &rest args)
-  "Make href attribute"
-  (xfactory:attributes :href
-                       (apply-format-aux format args)))
-
-(defun etitle (format &rest args)
-  (xfactory:with-element-factory ((E))
-    (E :head
-       (E :title
-          (xfactory:text (apply-format-aux format args))))))
-
-
-(defun index-page ()
-  (in-pool 
-   (xfactory:with-document-factory ((html))
-     (html :overlay
-           (etitle "Example 2: Index")
-           (html :div
-                 (xfactory:attributes :id "content")
-                 (eid "content")
-                 (html :h1 "Index")
-                 (html :ul
-                       (loop for x from 1 to 10
-                          do (html :li
-                                   (html :a
-                                         (ehref (genurl 'chapter-?.html :id x))
-                                         (xfactory:text "Chapter ~A" x))))))))))
-
-(define-simple-route root ("" :overlay-master *frame*)
-  (index-page))
-
-(define-simple-route index.html ("index.html" :overlay-master *frame*)
-  (index-page))
-
-(define-simple-route chapter-?.html ("chapter-:(id).html")
-  (in-pool
-   (xfactory:with-document-factory ((html))
-     (html :overlay
-           (etitle "Example 2: Chapter ~A" id)
-           (html :div
-                 (eid "content")
-                 (html :h1
-                       (xfactory:text "Chapter ~A" id))
-                 (html :ul
-                       (loop for x from 1 to 10
-                          do (html :li
-                                   (html :a
-                                         (ehref (genurl 'chapter-?-?.html :id1 id :id2 x))
-                                         (xfactory:text "Chapter ~A-~A" id x)))))
-                 (html :a
-                       (ehref (genurl 'root))
-                       "Back to Index"))))))
-
-(define-simple-route chapter-?-?.html ("chapter-:(id1)-:(id2).html" :overlay-master *frame*)
-  (in-pool
-   (xfactory:with-document-factory ((html))
-     (html :overlay
-           (etitle "Example 2: Chapter ~A-~A" id1 id2)
-           (html :div
-                 (eid "content")
-                 (html :h1
-                       (xfactory:text "Chapter ~A-~A" id1 id2))
-                 (html :p
-                       (xfactory:text "This is a chapter ~A-~A" id1 id2))
-                 (html :a
-                       (ehref (genurl 'chapter-?.html :id id1))
-                       (xfactory:text "Back to Chapter ~A" id1)))))))
+(define-route chapter-?-?.html ("chapter-:(id1)-:(id2).html")
+  (who:with-html-output-to-string (out)
+    (:html
+     (:head
+      (:title (who:fmt "Example 2. Chapter ~A-~A" id1 id2)))
+     (:body
+      (:h1 (who:fmt "Chapter ~A-~A" id1 id2))
+      (:p (who:fmt "This is a chapter ~A-~A" id1 id2))
+      ((:a :href (genurl 'chapter-?.html :id id1))
+       (who:fmt "Back to Chapter ~A" id1))))))
   
-          
-
-(restas:reconnect-all-plugins)
-(restas:start-web-server 8080)
+(restas:start-site :restas.example-2 :port 8080)
