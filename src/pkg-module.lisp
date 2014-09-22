354ba550a0e5176af608bb5f43873420963bb12d
Andre Miranda <andremiramor@gmail.com>
2014-09-22 15:31:58 -0300

    removed empty pkg-module.lisp file

--- src/pkg-module.lisp
+++
@@ -1,9 +0,0 @@
-;;;; pkg-module.lisp
-;;;;
-;;;; This file is part of the RESTAS library, released under Lisp-LGPL.
-;;;; See file COPYING for details.
-;;;;
-;;;; Author: Moskvitin Andrey <archimag@gmail.com>
-
-(in-package #:restas)
-
